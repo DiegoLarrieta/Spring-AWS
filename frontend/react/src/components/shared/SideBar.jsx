619dede3d63e77db93bb9110597c301c64eb5cb9
DiegoLarrieta <diegolarrieta@gmail.com>
2025-03-24 18:50:22 -0600

    DeployFront

--- frontend/react/src/components/shared/SideBar.jsx
+++ frontend/react/src/components/shared/SideBar.jsx
@@ -31,6 +31,7 @@ import {
     FiUsers
 } from 'react-icons/fi';
 import {useAuth} from "../context/AuthContext.jsx";
+import DiegoCodeLogo from "../../assets/DiegoCode.png"
 
 const LinkItems = [
     {name: 'Home', route: '/dashboard', icon: FiHome},
@@ -85,7 +86,7 @@ const SidebarContent = ({onClose, ...rest}) => {
                 <Image
                     borderRadius='full'
                     boxSize='75px'
-                    src='https://user-images.githubusercontent.com/40702606/210880158-e7d698c2-b19a-4057-b415-09f48a746753.png'
+                    src={DiegoCodeLogo}
                     alt='Amigoscode'
                 />
                 <CloseButton display={{base: 'flex', md: 'none'}} onClick={onClose}/>
